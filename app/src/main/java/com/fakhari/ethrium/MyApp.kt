9f90eaadd80725c508dd72bfb8b9dab5b7073245
hassan <hasan@digilite.ca>
2023-08-17 22:53:49 +0330

    Project Struct

---
+++ app/src/main/java/com/fakhari/ethrium/MyApp.kt
@@ -0,0 +1,11 @@
+package com.fakhari.ethrium
+
+import android.app.Application
+
+//project application class
+class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
